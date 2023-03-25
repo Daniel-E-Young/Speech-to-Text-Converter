8f4b50a39c1c349e20d89b9025950203837e3943
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-03-25 10:39:23 +0000

    commit

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const recordBtn = document.querySelector (".record"),
     result = document.querySelector(".result"),
-    downloadBtn = document.querySelector
+    downloadBtn = document.querySelector("")
