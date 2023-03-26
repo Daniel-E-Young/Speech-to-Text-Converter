9cfc53eb80b22809e0df4875be8ba4a3ec48e1eb
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-03-26 16:17:53 +0000

    commit

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ const recordBtn = document.querySelector(".record"),
     result = document.querySelector(".result"),
     downloadBtn = document.querySelector(".download"),
     inputLanhuage = document.querySelector("#language"),
-    clearBtn = document.querySelector(".clear")
+    clearBtn = document.querySelector(".clear");
+
+    
