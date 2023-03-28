5fb897bb9fa4fd092868ff7df37036e81b390e0e
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-03-28 05:08:19 +0000

    commit

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ let SpeechRecognition =
             recordBtn.querySelector("p").innerHTML = "Listening...";
             recognition.start();
             recognition.onresult = (event) => {
-                const speechResult = 
+                const speechResult = event.result
             }
         }
     }
