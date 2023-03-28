e27f5fb583dc1108d4b43dfdfccc29fdbd538462
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-03-28 05:00:06 +0000

    commit

--- script.js
+++ script.js
@@ -23,6 +23,6 @@ let SpeechRecognition =
         try {
             recognition = new SpeechRecognition();
             recognition.lang = inputLanguage.value;
-            recognition.interim
+            recognition.interimReslut
         }
     }
