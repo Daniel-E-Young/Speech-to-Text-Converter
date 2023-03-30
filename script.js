8e6797a765fdd2ab04f6e0f6c576835a3be0d71a
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-03-30 06:49:06 +0000

    commit

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ let SpeechRecognition =
                 } else {
                     //creative p with class interim if not already there
                     if (!document.querySelector(".interim")) }
+                    const 
                 }
 
             }
