3e49fe86d1e3ce07e02d25ff62bcfaa938098b17
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-03-31 10:43:42 +0000

    commit

--- script.js
+++ script.js
@@ -46,5 +46,6 @@ let SpeechRecognition =
             downloadBtn.disabled = false;
      };
      recognition.onspeechend = () => {
-        speechToText
-     }
+        speechToText();
+     };
+     recognition
