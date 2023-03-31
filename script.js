a9e4ce5eb62b8b1d9d5dadc8d596d72a96c55fac
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-03-31 10:46:09 +0000

    commit

--- script.js
+++ script.js
@@ -51,6 +51,6 @@ let SpeechRecognition =
      recognition.onerror = (event)=> {
         stopRecording();
         if(event.error=== "no-speech"){
-            alert("No speech was detected. Stopping now...")
+            alert("No speech was detected. Ending now...")
         }
      } 
