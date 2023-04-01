556550c81927857eeccd35ff66a558a6d393b82d
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-04-01 05:32:22 +0000

    commit

--- script.js
+++ script.js
@@ -59,6 +59,6 @@ let SpeechRecognition =
         } else if(event.error === "not-allowed"){
             alert("Permission to use microphone is blocked.");
         } else if (event.error === "aborted") {
-            al
+            alert()
         }
      } 
