feb0ad2b4445399c20c9a9d5612a0f26f3ffe52c
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-04-03 06:10:59 +0000

    commit

--- script.js
+++ script.js
@@ -90,5 +90,5 @@ function download() {
     );
     element.setAttribute("download", filename);
     element.style.display = "none";
-    document.body.appendChild
+    document.body.appendChild(element)
 }
